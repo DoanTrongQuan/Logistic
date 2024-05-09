62345387a8e1604fad1417871ee9b6bbdd8a913a
TrongQuan <142089306+DoanTrongQuan@users.noreply.github.com>
2024-05-09 08:33:13 +0700

    .

--- pages/home.vue
+++ pages/home.vue
@@ -1,10 +1,47 @@
 <template>
-<div>
-  <div>
-
+<div class=e880_11780><span  class="e880_11781">Tracking code</span>
+  <div class=e880_11782>
+    <div class=e880_11783><span  class="e880_11784">1.</span><span  class="e880_11785">LB0000000123456</span>
+      <div class=e880_11786>
+        <div class=ei880_11786_10_26888>
+          <div class=ei880_11786_10_26889>
+            <div class=ei880_11786_10_26890>
+              <div class="ei880_11786_10_26891"></div>
+              <div class="ei880_11786_10_26892"></div>
+            </div>
+            <div class="ei880_11786_10_26893"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class=e880_11787><span  class="e880_11788">2.</span><span  class="e880_11789"> </span></div>
+  </div><span  class="e880_11790">1/40</span>
+  <div class=e880_11791>
+    <div class=e880_11792>
+      <div class=ei880_11792_10_25424>
+        <div class="ei880_11792_10_25425"></div>
+      </div>
+    </div>
   </div>
-  <div>
-
+  <div class=e880_11793><span  class="ei880_11793_38_2944">Track</span>
+    <div class=ei880_11793_38_2952>
+      <div class=ei880_11793_38_2952_10_17412>
+        <div class=ei880_11793_38_2952_10_17413>
+          <div class=ei880_11793_38_2952_10_17414>
+            <div class=ei880_11793_38_2952_10_17415>
+              <div class="ei880_11793_38_2952_10_17416"></div>
+              <div class="ei880_11793_38_2952_10_17417"></div>
+            </div>
+            <div class="ei880_11793_38_2952_10_17418"></div>
+          </div>
+          <div class=ei880_11793_38_2952_10_17419>
+            <div class="ei880_11793_38_2952_10_17420"></div>
+            <div class="ei880_11793_38_2952_10_17421"></div>
+          </div>
+          <div class="ei880_11793_38_2952_10_17422"></div>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 </template>
@@ -13,6 +50,336 @@
 </script>
 
 <style scoped>
+.880_11780 { 
+	overflow:hidden;
+	border:0.5px solid rgba(228.9563038945198, 228.9563038945198, 228.9563038945198, 1);
+}
+.e880_11780 { 
+	background-color:rgba(255, 255, 255, 1);
+	width:389px;
+	height:338px;
+	border-top-left-radius:32px;
+	border-top-right-radius:32px;
+	border-bottom-left-radius:32px;
+	border-bottom-right-radius:32px;
+	position:absolute;
+}
+.e880_11781 { 
+	color:rgba(23.057721331715584, 23.34417276084423, 30.21899376064539, 1);
+	width:133px;
+	height:32px;
+	position:absolute;
+	left:28px;
+	top:24px;
+	font-family:Mulish;
+	text-align:left;
+	font-size:20px;
+	letter-spacing:0;
+	line-height:px;
+}
+.e880_11782 { 
+	width:389px;
+	height:88px;
+	position:absolute;
+	left:0px;
+	top:76px;
+}
+.e880_11783 { 
+	background-color:rgba(244.98068243265152, 244.98068243265152, 249.900004863739, 1);
+	width:389px;
+	height:44px;
+	position:absolute;
+	left:0px;
+	top:0px;
+}
+.e880_11784 { 
+	color:rgba(23.057721331715584, 23.34417276084423, 30.21899376064539, 1);
+	width:12px;
+	height:20px;
+	position:absolute;
+	left:28px;
+	top:12px;
+	font-family:Mulish;
+	text-align:left;
+	font-size:14px;
+	letter-spacing:0;
+	line-height:px;
+}
+.e880_11785 { 
+	color:rgba(116.08979746699333, 117.29889258742332, 146.31786704063416, 1);
+	width:285px;
+	height:20px;
+	position:absolute;
+	left:48px;
+	top:12px;
+	font-family:Mulish;
+	text-align:left;
+	font-size:14px;
+	letter-spacing:0;
+	line-height:px;
+}
+.e880_11786 { 
+	width:20px;
+	height:20px;
+	position:absolute;
+	left:341px;
+	top:12px;
+}
+.ei880_11786_10_26888 { 
+	width:20px;
+	height:20px;
+	position:absolute;
+	left:0px;
+	top:0px;
+}
+.ei880_11786_10_26889 { 
+	width:20px;
+	height:20px;
+	position:absolute;
+	left:0px;
+	top:0px;
+}
+.ei880_11786_10_26890 { 
+	width:11.666637420654297px;
+	height:11.665882110595703px;
+	position:absolute;
+	left:4.166725158691406px;
+	top:4.166666507720947px;
+}
+.ei880_11786_10_26891 { 
+	transform: rotate(45deg);
+	width:16.498048782348633px;
+	height:0px;
+	position:absolute;
+	left:0.000755170127376914px;
+	top:0px;
+}
+.i880_11786_10_26891 { 
+	border:1px solid rgba(116.08979746699333, 117.29889258742332, 146.31786704063416, 1);
+}
+.ei880_11786_10_26892 { 
+	transform: rotate(45deg);
+	position:absolute;
+	left:11.665882110595703px;
+	top:0px;
+}
+.i880_11786_10_26892 { 
+	border:1px solid rgba(116.08979746699333, 117.29889258742332, 146.31786704063416, 1);
+}
+.ei880_11786_10_26893 { 
+	width:20px;
+	height:20px;
+	position:absolute;
+	left:0px;
+	top:0px;
+}
+.e880_11787 { 
+	width:389px;
+	height:44px;
+	position:absolute;
+	left:0px;
+	top:44px;
+}
+.e880_11788 { 
+	color:rgba(23.057721331715584, 23.34417276084423, 30.21899376064539, 1);
+	width:12px;
+	height:20px;
+	position:absolute;
+	left:28px;
+	top:12px;
+	font-family:Mulish;
+	text-align:left;
+	font-size:14px;
+	letter-spacing:0;
+	line-height:px;
+}
+.e880_11789 { 
+	color:rgba(116.08979746699333, 117.29889258742332, 146.31786704063416, 1);
+	width:313px;
+	height:20px;
+	position:absolute;
+	left:48px;
+	top:12px;
+	font-family:Mulish;
+	text-align:left;
+	font-size:14px;
+	letter-spacing:0;
+	line-height:px;
+}
+.e880_11790 { 
+	color:rgba(116.08979746699333, 117.29889258742332, 146.31786704063416, 1);
+	width:30px;
+	height:20px;
+	position:absolute;
+	left:331px;
+	top:30px;
+	font-family:Mulish;
+	text-align:left;
+	font-size:14px;
+	letter-spacing:0;
+	line-height:px;
+}
+.e880_11791 { 
+	background-color:rgba(255, 255, 255, 1);
+	width:48px;
+	height:48px;
+	position:absolute;
+	left:313px;
+	top:262px;
+	border-top-left-radius:12px;
+	border-top-right-radius:12px;
+	border-bottom-left-radius:12px;
+	border-bottom-right-radius:12px;
+}
+.880_11791 { 
+	border:1px solid rgba(210.6761384010315, 215.7295024394989, 228.72387796640396, 1);
+}
+.e880_11792 { 
+	width:24px;
+	height:24px;
+	position:absolute;
+	left:12px;
+	top:12px;
+}
+.ei880_11792_10_25424 { 
+	width:24px;
+	height:24px;
+	position:absolute;
+	left:0px;
+	top:0px;
+}
+.i880_11792_10_25425 { 
+	border:1px solid rgba(231.5087941288948, 22.645960226655006, 60.241268277168274, 1);
+}
+.ei880_11792_10_25425 { 
+	width:20px;
+	height:20px;
+	position:absolute;
+	left:2px;
+	top:1.999999761581421px;
+}
+.e880_11793 { 
+	background-color:rgba(255, 117.91666343808174, 20.000000707805157, 1);
+	width:273px;
+	height:48px;
+	position:absolute;
+	left:28px;
+	top:262px;
+	border-top-left-radius:12px;
+	border-top-right-radius:12px;
+	border-bottom-left-radius:12px;
+	border-bottom-right-radius:12px;
+}
+.ei880_11793_38_2944 { 
+	color:rgba(255, 255, 255, 1);
+	width:41px;
+	height:24px;
+	position:absolute;
+	left:97px;
+	top:12px;
+	font-family:Mulish;
+	text-align:left;
+	font-size:16px;
+	letter-spacing:0;
+	line-height:px;
+}
+.ei880_11793_38_2952 { 
+	width:24px;
+	height:24px;
+	position:absolute;
+	left:152px;
+	top:12px;
+}
+.ei880_11793_38_2952_10_17412 { 
+	width:24px;
+	height:24px;
+	position:absolute;
+	left:0px;
+	top:0px;
+}
+.ei880_11793_38_2952_10_17413 { 
+	width:24px;
+	height:24px;
+	position:absolute;
+	left:0px;
+	top:0px;
+}
+.ei880_11793_38_2952_10_17414 { 
+	width:19.219970703125px;
+	height:20.000019073486328px;
+	position:absolute;
+	left:2.3900146484375px;
+	top:2px;
+}
+.ei880_11793_38_2952_10_17415 { 
+	width:17.5999755859375px;
+	height:14.16998291015625px;
+	position:absolute;
+	left:0.779998779296875px;
+	top:5.44000244140625px;
+}
+.i880_11793_38_2952_10_17416 { 
+	border:1px solid rgba(255, 255, 255, 1);
+}
+.ei880_11793_38_2952_10_17416 { 
+	width:17.5999755859375px;
+	height:5.1099853515625px;
+	position:absolute;
+	left:0px;
+	top:0px;
+}
+.i880_11793_38_2952_10_17417 { 
+	border:1px solid rgba(255, 255, 255, 1);
+}
+.ei880_11793_38_2952_10_17417 { 
+	position:absolute;
+	left:8.829986572265625px;
+	top:5.0999755859375px;
+}
+.i880_11793_38_2952_10_17418 { 
+	border:1px solid rgba(255, 255, 255, 1);
+}
+.ei880_11793_38_2952_10_17418 { 
+	width:19.219970703125px;
+	height:20.000019073486328px;
+	position:absolute;
+	left:0px;
+	top:0px;
+}
+.ei880_11793_38_2952_10_17419 { 
+	width:7px;
+	height:7px;
+	position:absolute;
+	left:16px;
+	top:15px;
+}
+.i880_11793_38_2952_10_17420 { 
+	border:1px solid rgba(255, 255, 255, 1);
+}
+.ei880_11793_38_2952_10_17420 { 
+	width:6.399993896484375px;
+	height:6.4000244140625px;
+	position:absolute;
+	left:0px;
+	top:0px;
+}
+.i880_11793_38_2952_10_17421 { 
+	border:1px solid rgba(255, 255, 255, 1);
+}
+.ei880_11793_38_2952_10_17421 { 
+	width:1px;
+	height:1px;
+	position:absolute;
+	left:6px;
+	top:6px;
+}
+.ei880_11793_38_2952_10_17422 { 
+	width:24px;
+	height:24px;
+	position:absolute;
+	left:0px;
+	top:0px;
+}
 
 </style>
 
