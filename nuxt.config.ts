fb9b5beb1a29ce1f84daa1094236ea760bffe060
TrongQuan <142089306+DoanTrongQuan@users.noreply.github.com>
2024-05-13 17:34:39 +0700

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,4 +9,24 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+
+  app: {
+    head: {
+      title: 'Wealify',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: '' },
+      ],
+      link: [
+
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Mulish:wght@300;400;500;600;700;800&display=swap",
+        },
+    
+      ],
+    
+    },  
+  },
 })
